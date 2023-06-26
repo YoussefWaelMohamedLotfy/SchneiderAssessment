0c4d0ab0bdc99a0490b2f3156cb9b1776653f13a
Youssef Wael Mohamed-Lotfy Mousa <40206862+YoussefWaelMohamedLotfy@users.noreply.github.com>
2023-06-27 02:45:02 +0300

    Added Domain Layer Class Library

---
+++ src/Services/HR.Domain/Entities/VacationType.cs
@@ -0,0 +1,10 @@
+﻿using HR.Domain.Common;
+
+namespace HR.Domain.Entities;
+
+public sealed class VacationType : BaseEntity<int>
+{
+    public string Name { get; set; } = default!;
+
+    public List<VacationRequest> Requests { get; set; } = default!;
+}
