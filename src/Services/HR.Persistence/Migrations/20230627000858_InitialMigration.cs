2762614a3732c95734b482723d31c1c42f1d2e8b
Youssef Wael Mohamed-Lotfy Mousa <40206862+YoussefWaelMohamedLotfy@users.noreply.github.com>
2023-06-27 03:12:30 +0300

    Create Initial Migration for AppDbContext

---
+++ src/Services/HR.Persistence/Migrations/20230627000858_InitialMigration.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HR.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AnnualVacationRemaining = table.Column<int>(type: "int", nullable: false),
+                    SickVacationRemaining = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "VacationTypes",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VacationTypes", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "VacationRequests",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    StartDate = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    EndDate = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    RequestingEmployeeId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    VacationTypeId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VacationRequests", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_VacationRequests_Employees_RequestingEmployeeId",
+                        column: x => x.RequestingEmployeeId,
+                        principalTable: "Employees",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_VacationRequests_VacationTypes_VacationTypeId",
+                        column: x => x.VacationTypeId,
+                        principalTable: "VacationTypes",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VacationRequests_RequestingEmployeeId",
+                table: "VacationRequests",
+                column: "RequestingEmployeeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VacationRequests_VacationTypeId",
+                table: "VacationRequests",
+                column: "VacationTypeId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "VacationRequests");
+
+            migrationBuilder.DropTable(
+                name: "Employees");
+
+            migrationBuilder.DropTable(
+                name: "VacationTypes");
+        }
+    }
+}
