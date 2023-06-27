2289a5dcb0ed94c546b3859d18127c5fe1d6392a
Youssef Wael Mohamed-Lotfy Mousa <40206862+YoussefWaelMohamedLotfy@users.noreply.github.com>
2023-06-27 14:47:23 +0300

    Add new endpoint for Fetching single employee

---
+++ src/Services/HR.API/Controllers/EmployeesController.cs
@@ -0,0 +1,32 @@
+﻿using HR.Application.Features.EmployeeFeatures.Queries;
+
+using MediatR;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace HR.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public sealed class EmployeesController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public EmployeesController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    /// <summary>
+    /// Gets a single Employee by ID
+    /// </summary>
+    /// <param name="id"></param>
+    /// <param name="ct"></param>
+    /// <returns></returns>
+    [HttpGet("{id:guid}")]
+    public async Task<IActionResult> GetAllVacationTypes(Guid id, CancellationToken ct)
+    {
+        var result = await _mediator.Send(new GetEmployeeByIdQuery(id), ct);
+        return result is not null ? Ok(result) : NotFound();
+    }
+}
---
+++ src/Services/HR.Application/Features/EmployeeFeatures/Queries/GetEmployeeByIdQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using HR.Contracts.EmployeeContracts;
+using HR.Persistence.Data;
+
+using MediatR;
+
+namespace HR.Application.Features.EmployeeFeatures.Queries;
+
+public sealed record GetEmployeeByIdQuery(Guid Id) : IRequest<GetEmployeeDTO?>;
+
+internal sealed class GetEmployeeByIdQueryHandler : IRequestHandler<GetEmployeeByIdQuery, GetEmployeeDTO?>
+{
+    private readonly AppDbContext _context;
+
+    public GetEmployeeByIdQueryHandler(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<GetEmployeeDTO?> Handle(GetEmployeeByIdQuery request, CancellationToken cancellationToken)
+    {
+        var employee = await _context.Employees.FindAsync(new object[] { request.Id }, cancellationToken);
+
+        return employee is null
+            ? null
+            : new()
+        {
+            ID = employee.ID,
+            Name = employee.Name,
+            AnnualVacationRemaining = employee.AnnualVacationRemaining,
+            SickVacationRemaining = employee.SickVacationRemaining
+        };
+    }
+}
