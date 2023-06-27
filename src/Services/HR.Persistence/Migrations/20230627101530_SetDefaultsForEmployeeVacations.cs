fab5f66ccc29b90eb00e972c1f9ccc7cb18e8c03
Youssef Wael Mohamed-Lotfy Mousa <40206862+YoussefWaelMohamedLotfy@users.noreply.github.com>
2023-06-27 13:24:22 +0300

    Configue Defaults for Initial Vacations for Employee

---
+++ src/Services/HR.Persistence/Migrations/20230627101530_SetDefaultsForEmployeeVacations.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HR.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class SetDefaultsForEmployeeVacations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "SickVacationRemaining",
+                table: "Employees",
+                type: "int",
+                nullable: false,
+                defaultValue: 10,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AnnualVacationRemaining",
+                table: "Employees",
+                type: "int",
+                nullable: false,
+                defaultValue: 11,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "SickVacationRemaining",
+                table: "Employees",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldDefaultValue: 10);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AnnualVacationRemaining",
+                table: "Employees",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldDefaultValue: 11);
+        }
+    }
+}
