0cc54145eaf802cf21c02689fa6fff251fe8411a
Youssef Wael Mohamed-Lotfy Mousa <40206862+YoussefWaelMohamedLotfy@users.noreply.github.com>
2023-07-02 00:22:32 +0300

    Add new project for SDK

---
+++ src/SDKs/HR.SDK/ServiceRegistration.cs
@@ -0,0 +1,22 @@
+﻿using HR.SDK.Configuration;
+using HR.SDK.Interfaces;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using Refit;
+
+namespace HR.SDK;
+
+public static class ServiceRegistration
+{
+    public static IServiceCollection AddHRApiClients(this IServiceCollection services, Action<HRApiOptions> options)
+    {
+        HRApiOptions apiOptions = new();
+        options?.Invoke(apiOptions);
+
+        services.AddRefitClient<IVacationRequestsClient>()
+            .ConfigureHttpClient(c => c.BaseAddress = new Uri(apiOptions.BaseUrl));
+
+        return services;
+    }
+}
