7108c19383dd7b0bd3773f7d589cf223fe61de21
Youssef Wael Mohamed-Lotfy Mousa <40206862+YoussefWaelMohamedLotfy@users.noreply.github.com>
2023-07-02 00:23:02 +0300

    Add initial Client for Vacation Requests

---
+++ src/SDKs/HR.SDK/Clients/HRApiClient.cs
@@ -0,0 +1,11 @@
+﻿namespace HR.SDK.Endpoints;
+
+public sealed class HRApiClient
+{
+    public VacationRequests VacationRequests { get; }
+
+    public HRApiClient(VacationRequests vacationRequests)
+    {
+        VacationRequests = vacationRequests;
+    }
+}
---
+++ src/SDKs/HR.SDK/Endpoints/VacationRequests.cs
@@ -0,0 +1,26 @@
+﻿using HR.Contracts.VacationRequestContracts;
+using HR.SDK.Interfaces;
+
+using Refit;
+
+namespace HR.SDK.Endpoints;
+
+public sealed class VacationRequests
+{
+    private readonly IVacationRequestsClient _client;
+
+    public VacationRequests(IVacationRequestsClient client)
+    {
+        _client = client;
+    }
+
+    public async Task<IApiResponse<GetVacationRequestDTO>> GetVacationRequestByIDAsync(Guid id)
+    {
+        return await _client.GetVacationRequestById(id);
+    }
+
+    public async Task<IApiResponse<GetVacationRequestDTO>> CreateNewVacationRequestAsync(CreateVacationRequestDTO request)
+    {
+        return await _client.CreateNewVacationRequest(request);
+    }
+}
