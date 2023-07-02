893ed8f3a730bdb76142453d7edf6c2773f37e71
Youssef Wael Mohamed-Lotfy Mousa <40206862+YoussefWaelMohamedLotfy@users.noreply.github.com>
2023-07-02 23:30:24 +0300

    Add new Component for Employee Details

--- src/Shared/HR.Razor.Components/Pages/Employees.razor.cs
+++ src/Shared/HR.Razor.Components/Pages/Employees.razor.cs
@@ -1,35 +1,11 @@
-using HR.Contracts.VacationRequestContracts;
-using HR.Contracts.VacationTypeContracts;
-
 namespace HR.Razor.Components.Pages;
 
-public partial class Employees
+public sealed partial class Employees
 {
-    private CreateVacationRequestDTO _newVacationrequest;
-    private List<GetVacationTypeDTO> _vacationTypes;
 
-    public Employees()
+
+    protected override Task OnInitializedAsync()
     {
-        _newVacationrequest = new()
-        {
-            StartDate = DateTime.Now,
-            EndDate = DateTime.Now.AddDays(1),
-        };
-    }
-
-    protected override async Task OnInitializedAsync()
-    {
-        var response = await _hrClient.VacationTypes.GetAllVacationTypesAsync();
-
-        if (response.IsSuccessStatusCode)
-        {
-            _vacationTypes = response.Content!;
-        }
-
-    }
-
-    private async void SubmitNewVacationRequest()
-    {
-        var response = await _hrClient.VacationRequests.CreateNewVacationRequestAsync(_newVacationrequest);
+        return base.OnInitializedAsync();
     }
 }
