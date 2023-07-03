83127dee2544ac69d51755edb8b7af467a1d1513
Youssef Wael Mohamed-Lotfy Mousa <40206862+YoussefWaelMohamedLotfy@users.noreply.github.com>
2023-07-03 18:12:04 +0300

    Improved Employees Index Page

--- src/Shared/HR.Razor.Components/Pages/Employees.razor.cs
+++ src/Shared/HR.Razor.Components/Pages/Employees.razor.cs
@@ -1,11 +1,18 @@
+using HR.Contracts.EmployeeContracts;
+
 namespace HR.Razor.Components.Pages;
 
 public sealed partial class Employees
 {
+    private List<GetEmployeeDTO> _employees;
 
-
-    protected override Task OnInitializedAsync()
+    protected override async Task OnInitializedAsync()
     {
-        return base.OnInitializedAsync();
+        var response = await _hrClient.Employees.GetAllEmployeesAsync();
+
+        if (response.IsSuccessStatusCode)
+        {
+            _employees = response.Content!;
+        }
     }
 }
